535daa12c2aef6331051a1f711c7e1726032d126
Jaden Leake <jadenleake333@gmail.com>
2021-02-25 00:03:22 -0500

    Started making calculations

--- draw.py
+++ draw.py
@@ -1,26 +1,26 @@
 import pygame
 import sys
 
-def initialize_board(**kwargs):
+def initialize_board(data):
     pygame.init()
     pygame.font.init()
     my_font = pygame.font.Font(None, 30)
-    size = kwargs['width']+100, kwargs['height']+100
+    size = data['width']+100, data['height']+100
     screen = pygame.display.set_mode(size)
     red = (255,0,0)
     white = (255,255,255)
     green = (0,255,0)
-    for points in kwargs['nodes']:
+    for points in data['nodes']:
         point = pygame.draw.circle(screen,green,(points['x'],points['y']),10)
         point_id = my_font.render(str(points['id']),False,white)
         pygame.Surface.blit(screen, point_id, (points['x']-5, points['y']-5))
     
-    for i in range(kwargs['num']):
-        for j in range(len(kwargs['edges'])):
-            if i == kwargs['edges'][j]['source']:
-                target = kwargs['edges'][j]['target']
-                x,y = kwargs['nodes'][i]['x'],kwargs['nodes'][i]['y']
-                x2,y2 = kwargs['nodes'][target]['x'],kwargs['nodes'][target]['y']
+    for i in range(data['num']):
+        for j in range(len(data['edges'])):
+            if i == data['edges'][j]['source']:
+                target = data['edges'][j]['target']
+                x,y = data['nodes'][i]['x'],data['nodes'][i]['y']
+                x2,y2 = data['nodes'][target]['x'],data['nodes'][target]['y']
                 point = pygame.draw.line(screen,red,(x,y),(x2,y2),1)
 
     while 1:
--- main.py
+++ main.py
@@ -1,27 +1,64 @@
+#Author: Jaden Leake
+#Date: 2/23/2021
+#Attempted solution to travelling salesman problem #
+
 import sys
-import random
 import json
 import math
 from draw import *
 
-def calculate_distance(**kwargs):
-    dist_list = []
-    for i in range(kwargs['num']-1):
-        for j in range(len(kwargs['edges'])):
-            if i == kwargs['edges'][j]['source']: # This assumes that the id is the same as the index which may not always be true
-                target = kwargs['edges'][j]['target']
-                x,y = kwargs['nodes'][i]['x'],kwargs['nodes'][i]['y']
-                x2,y2 = kwargs['nodes'][target]['x'],kwargs['nodes'][target]['y']
+def calculate_distance(data):
+    dist_dict = {}
+    dist_dict['distances'] = []
+    for i in range(data['num']-1):
+        for j in range(len(data['edges'])):
+            if i == data['edges'][j]['source']: # This assumes that the id is the same as the index which may not always be true
+                target = data['edges'][j]['target']
+                x,y = data['nodes'][i]['x'],data['nodes'][i]['y']
+                x2,y2 = data['nodes'][target]['x'],data['nodes'][target]['y']
                 distance = math.dist([x,y],[x2,y2])
-                dist_list.append((i,target,distance))
-    return dist_list
+                dist_dict['distances'].append({
+                                                'source':i,
+                                                'target':target,
+                                                'distance':distance
+                                             })
+    return dist_dict
+
+def solve_problem(start,end,data):
+    #Start from 1
+    #Find the shortest node from 1
+    #Find the shortest from that node...#
+    start_idx = 0
+    for idx,i in enumerate(data['distances']):
+        if i['source'] == start:
+            start_idx = idx
+            break
+
+    min_dist = data['distances'][start_idx]['distance']
+    index = data['distances'][start_idx]
+    #print(min_dist)
+    #print(index)
+    new_target = 0
+    for i in range(start_idx,len(data['distances'])):
+        #if i > data['distances'][i]['source']:
+        #    break
+
+        if data['distances'][i]['distance'] < min_dist:
+            min_dist = data['distances'][i]['distance']
+            index = data['distances'][i]
+
+    tsp['solution'].append(index)
 
 if __name__ == "__main__":
+    global tsp
+    tsp = {}
+    tsp['solution'] = []
     try:
         jsonfile = sys.argv[1]
+        visualize = sys.argv[2]
     except:
         jsonfile = None
-        print("Enter a .json filename")
+        print("Enter a .json file and bool value")
         quit(0)
     
     if jsonfile != None:
@@ -31,7 +68,10 @@ if __name__ == "__main__":
         except:
             print("Please enter a valid .json filename or make sure you have input a valid json")
             quit()
-        distances = calculate_distance(**data)
-        initialize_board(**data)
+        distances = calculate_distance(data)
+        solve_problem(5,data['num']-2,distances)
+        print(tsp)
+        if visualize == "True":
+            initialize_board(data)
         
         
